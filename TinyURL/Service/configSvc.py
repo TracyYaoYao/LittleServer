0915bff6a524116605df09076adaae120291a381
TracyYaoYao <18314464415@163.com>
2021-07-16 23:18:57 +0800

    modify url

--- TinyURL/Service/configSvc.py
+++ TinyURL/Service/configSvc.py
@@ -1,7 +1,7 @@
 class configSvcImpl:
     def __init__(self):
         self.protocol = "http"
-        self.domain = "kiki.binacs.cn"
+        self.domain = "kiki.zone"
         # self.domain = "127.0.0.1:8000"
 
     def GetDomainSite(self):
